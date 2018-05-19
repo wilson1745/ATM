68bc2edfe5c1c9ed6254ca53daabe400d65d439c
wilson1745 <wilson-power0130@hotmail.com>
2018-05-19 19:46:03 +0100

    spinner & adapter

--- app/src/main/java/com/tom/atm/LoginActivity.java
+++ app/src/main/java/com/tom/atm/LoginActivity.java
@@ -12,31 +12,31 @@ public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        EditText edUserid = findViewById(R.id.ed_userid);
-        SharedPreferences setting =
-                getSharedPreferences("atm", MODE_PRIVATE);
-        edUserid.setText(setting.getString("PREF_USERID", ""));
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_login);
+       EditText edUserid = findViewById(R.id.ed_userid);
+       SharedPreferences setting = getSharedPreferences("atm", MODE_PRIVATE);
+       edUserid.setText(setting.getString("PREF_USERID", ""));
     }
 
-    public void login(View view){
-        EditText edUserid = findViewById(R.id.ed_userid);
-        EditText edPasswd = findViewById(R.id.ed_passwd);
-        String uid = edUserid.getText().toString();
-        String pw = edPasswd.getText().toString();
-        if (uid.equals("jack") && pw.equals("1234")){ //登入成功
-            SharedPreferences setting =
-                    getSharedPreferences("atm", MODE_PRIVATE);
-            setting.edit()
-                    .putString("PREF_USERID", uid)
-                    .apply();
+    public void login(View view) {
+       EditText edUserid = findViewById(R.id.ed_userid);
+       EditText edPasswd = findViewById(R.id.ed_passwd);
+       String uid = edUserid.getText().toString();
+       String pw = edPasswd.getText().toString();
+
+       if(uid.equals("jack") && pw.equals("1234")) { //登入成功
+            SharedPreferences setting = getSharedPreferences("atm", MODE_PRIVATE);
+            setting.edit().putString("PREF_USERID", uid).apply();
+
             Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show();
             getIntent().putExtra("LOGIN_USERID", uid);
             getIntent().putExtra("LOGIN_PASSWD", pw);
             setResult(RESULT_OK, getIntent());
             finish();
-        }else{  //登入失敗
+        }
+
+        else {  //登入失敗
             new AlertDialog.Builder(this)
                         .setTitle("Atm")
                         .setMessage("登入失敗")
--- app/src/main/java/com/tom/atm/MainActivity.java
+++ app/src/main/java/com/tom/atm/MainActivity.java
@@ -14,73 +14,66 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public static final int RC_LOGIN = 1;
-    boolean logon = false;
-    String[] func = {"餘額查詢", "交易明細", "最新消息", "投資理財", "離開"};
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //使用Spinner
-        Spinner notify = findViewById(R.id.spinner);
-        final ArrayAdapter nAdapter = ArrayAdapter.createFromResource(
-                this, R.array.notify_array, android.R.layout.simple_spinner_item);
-        nAdapter.setDropDownViewResource(
-                android.R.layout.simple_spinner_dropdown_item);
-        notify.setAdapter(nAdapter);
-        notify.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this,
-                        nAdapter.getItem(position).toString(), Toast.LENGTH_LONG).show();
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+   public static final int RC_LOGIN = 1;
+   boolean logon = false;
+   String[] func = {"餘額查詢", "交易明細", "最新消息", "投資理財", "離開"};
 
-            }
-        });
-        //使用ListView
-        ListView list = findViewById(R.id.list);
-        ArrayAdapter adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, func);
-        list.setAdapter(adapter);
-        //測試TestActivity
-//        startActivity(new Intent(this, TestActivity.class));
-        if (!logon){ //如未登入, 則開啟LoginActivity
-            Intent intent = new Intent(this, LoginActivity.class);
-//            startActivity(intent);
-            startActivityForResult(intent, RC_LOGIN);
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+
+      //使用Spinner
+      Spinner notify = findViewById(R.id.spinner);
+      ArrayAdapter<CharSequence> nAdapter = ArrayAdapter.createFromResource(
+              this, R.array.notify_array, android.R.layout.simple_spinner_item);
+      nAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+      notify.setAdapter(nAdapter);
+
+      //使用ListView
+      ListView list = findViewById(R.id.list);
+      ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, func);
+      list.setAdapter(adapter);
+
+      //測試TestActivity
+      //startActivity(new Intent(this, TestActivity.class));
+
+      if(!logon){ //如未登入, 則開啟LoginActivity
+         Intent intent = new Intent(this, LoginActivity.class);
+         //startActivity(intent);
+         startActivityForResult(intent, RC_LOGIN);
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      getMenuInflater().inflate(R.menu.menu_main, menu);
+      return true;
+   }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_setting){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+      int id = item.getItemId();
+
+      if (id == R.id.action_setting) {
+         return true;
+      }
+      return super.onOptionsItemSelected(item);
 
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_LOGIN){
-            if (resultCode == RESULT_OK){
-                String uid = data.getStringExtra("LOGIN_USERID");
-                String pw = data.getStringExtra("LOGIN_PASSWD");
-                Log.d("RESULT", uid + "/" + pw);
-            }else{
-                finish();
-            }
-        }
-    }
+   @Override
+   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+      super.onActivityResult(requestCode, resultCode, data);
+      if(requestCode == RC_LOGIN) {
+         if(resultCode == RESULT_OK) {
+            String uid = data.getStringExtra("LOGIN_USERID");
+            String pw = data.getStringExtra("LOGIN_PASSWD");
+            Log.d("RESULT", uid + "/" + pw);
+         }
+         else {
+            finish();
+         }
+      }
+   }
 }
